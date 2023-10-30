b240c2b2df0bb6df9916332f4db8c49e7ed7922f
deepak15deadsec <deepak15arvd@gmail.com>
2023-10-30 14:20:33 +0530

    styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,4 +11,23 @@ module.exports = {
     }
   },
   plugins: [],
+
+  theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1601px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1701px',
+      // => @media (min-width: 1536px) { ... }
+    }
+  }
 }
